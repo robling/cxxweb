d4b5f06765f96026108aaee37ccaac68318cd628
Robin.L <spdf@live.com>
2015-08-24 09:40:31 +0800

    sync

--- include/http.hpp
+++ include/http.hpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <unordered_set>
 
 using std::cout;
 using std::endl;
@@ -7,6 +8,24 @@ using boost::asio::ip::tcp;
 
 namespace cxxweb
 {
+	class session
+	{
+	public:
+		explicit session(boost::asio::io_service& ioservice);
+	};
+
+	class session_manager
+	{
+		typedef std::shared_ptr<session> session_ptr;
+	public:
+		session_ptr start();
+		void stop(session_ptr s);
+		void stop_all();
+		size_t get_count();
+	private:
+		std::unordered_set<session_ptr> session_pool;
+	};
+
 	class http
 	{
 	public:
@@ -14,5 +33,7 @@ namespace cxxweb
 		void run();
 		void use();
 	private:
+		boost::asio::io_service ioservice_;
+		tcp::acceptor accepter_;
 	};
 }
--- src/http.cpp
+++ src/http.cpp
@@ -1,1 +1,17 @@
-#include <http.hpp>
+#include "..\include\http.hpp"
+
+cxxweb::http::http(int port) :
+	ioservice_(),
+	accepter_(ioservice_, tcp::endpoint(tcp::v4(), port))
+{
+
+}
+
+void cxxweb::http::run()
+{
+	ioservice_.run();
+}
+
+cxxweb::session::session(boost::asio::io_service & ioservice)
+{
+}
